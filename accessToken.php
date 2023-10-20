01360e011f8164be6f5fa6c73c612a704c1b7a86
gradhav001 <jesse.richard@strathmore.edu>
2023-10-20 11:01:25 +0300

    began to do the stk Push

---
+++ accessToken.php
@@ -0,0 +1,24 @@
+<?php
+$customer_key = 'q15udd8pBCxKeMf3VKTOhwfsX63AGdT7';
+$customer_secret = 'pXuMzY4pD2oQaLCB';
+$access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+$headers = ['Content-Type:application/json; charset=utf8'];
+$curl = curl_init($access_token_url);
+
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER,TRUE);
+curl_setopt($curl, CURLOPT_HEADER,FALSE);
+curl_setopt($curl, CURLOPT_USERPWD, $customer_key . ";" . $customer_secret);
+
+$result = curl_exec($curl);
+$status = curl_getinfo($curl , CURLINFO_HTTP_CODE);
+
+
+// echo $result;
+
+$result = json_decode($result);
+$access_token = $result->access_token;
+curl_close($curl);
+
+
+?>
---
+++ stkPush.php
@@ -0,0 +1,7 @@
+<?php
+include 'accessToken.php';
+
+$date_default_timezone_set('Africa/Nairobi');
+$processRequestUrl = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+$callbackUrl = ""
+?>
